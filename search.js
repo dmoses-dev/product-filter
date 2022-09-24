7aad67e1e9732376d26f3e62701aa588a363d941
Dahunsi <ogundipeoluwadahunsi@gmail.com>
2022-09-24 01:38:22 -0700

    modified some files

--- search.js
+++ search.js
@@ -1,17 +1,12 @@
-// Select search and select product name
 const search = document.getElementById("search")
 const products = document.querySelectorAll('.product h4')
 
 search.addEventListener("keyup", mySearch)
 
-// Create a function called my search
 function mySearch(e){
     const myText = e.target.value.toLowerCase()
-    console.log(myText);
 
-// Loop through the array
 products.forEach((item)=>{
-    // console.log(item.textContent.toLowerCase());
     if(item.textContent.toLowerCase().indexOf(myText) !== -1){
         item.parentElement.style.display = "block"
     }
